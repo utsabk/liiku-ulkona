6a9e0392d32cb1edeab5d1e0b123180c3c1aa909
utsabk <kharelutsab@gmail.com>
2021-09-27 13:04:59 +0300

    Get current location  - Custom current location button  - Current location coords using expo-location  - Marker on Map with same coordinates

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,17 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import { Platform, Text, View, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import MapView from 'react-native-maps';
+import { Platform, View, Pressable, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
-import * as Location from 'expo-location';
-
+import {
+  Accuracy,
+  requestForegroundPermissionsAsync,
+  watchPositionAsync,
+} from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const styles = StyleSheet.create({
   map: {
-    height: '100%',
-    padding: 20,
-
+    ...StyleSheet.absoluteFillObject,
   },
-
+  myLocationButton: {
+    backgroundColor: '#FFFFFF',
+    position: 'absolute',
+    bottom: '10%',
+    right: '5%',
+    padding: 15,
+    elevation: 3,
+    borderRadius: 50,
+  },
 });
 
 const regionFinland = {
@@ -22,54 +32,48 @@ const regionFinland = {
 };
 
 const Maps = () => {
-  const [mapWidth, setMapWidth] = useState('99%');
   const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
 
-  // Update map style to force a re-render to make sure the geolocation button appears
-  const updateMapStyle = () => {
-    setMapWidth('100%');
+  const getCurrentPosition = async () => {
+    try {
+      if (Platform.OS === 'android' && !Constants.isDevice) {
+        throw new Error(
+          'Oops, This will not work in an Android emulator. Try it on your device!'
+        );
+      }
+      let { status } = await requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        throw new Error('Permission to access location was denied');
+      }
+
+      await watchPositionAsync(
+        { accuracy: Accuracy.BestForNavigation, distanceInterval: 2000 },
+        (position) => {
+          console.log('Position', position.coords);
+          setLocation(position.coords);
+        }
+      );
+    } catch (error) {
+      console.log('Error:-', error);
+    }
   };
 
-  useEffect(() => {
-    (async () => {
-      if (Platform.OS === 'android' && !Constants.isDevice) {
-        setErrorMsg(
-          'Oops, this will not work on Snack in an Android emulator. Try it on your device!'
-        );
-        return;
-      }
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  }, []);
-
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
-
-  console.log('Current Location:-', text);
-  
   return (
-      <MapView
-        style={[styles.map, { width: mapWidth }]}
-        provider={PROVIDER_GOOGLE}
-        showsUserLocation
-        showsMyLocationButton
-        followsUserLocation
-        zoomControlEnabled
-        onMapReady={updateMapStyle}
-        region={regionFinland}
-      />
+    <View style={StyleSheet.absoluteFillObject}>
+      <MapView style={styles.map} region={regionFinland}>
+        {location && (
+          <MapView.Marker
+            coordinate={{
+              latitude: location.latitude,
+              longitude: location.longitude,
+            }}
+          />
+        )}
+      </MapView>
+      <Pressable style={styles.myLocationButton} onPress={getCurrentPosition}>
+        <MaterialIcons name="my-location" size={24} color="grey" />
+      </Pressable>
+    </View>
   );
 };
 
